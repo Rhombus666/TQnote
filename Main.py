b6dd07ce48d6ce7a7b05e2a68b6ea48a90005125
Rhombus666 <Fergs.byrne@gmail.com>
2024-10-24 14:36:22 +1300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,2 +1,18 @@
 Name = input("What is your name?")
-print(hi'Name')
+print("Hi", Name)
+while True:
+    USRinput = input('command: ') #stuff
+    
+    if USRinput == 'note': #more stuff
+        NOTE1 = input("Note: ")
+        print(f"Note saved: {NOTE1}")
+    
+    elif USRinput == 'txt':
+        NOTESAVE = input("Note: ")
+        if NOTESAVE: 
+            with open("notes.txt", "a") as file:
+                file.write(NOTESAVE + "\n")
+                print("Note saved")
+    
+    else:
+        print("Unknown command")
